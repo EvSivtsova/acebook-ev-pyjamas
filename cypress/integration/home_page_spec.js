fcb6afd9e279624e7222a8eb2d39342766af070b
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-10-25 12:56:10 +0100

    Add integration tests for home/index view

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,8 +1,25 @@
-// const mongoosey = require("mongoose");
-
 describe("Home page", () => {
+  beforeEach(() => {
+    cy.visit('/');
+  })
+
   it("has a title", () => {
-    cy.visit("/");
     cy.get(".title").should("contain", "Acebook");
+  });
+
+  it("has a sign up button", () => {
+    cy.get('form').within(($form) => {
+      cy.intercept('get', '/users/new')
+      cy.contains('Sign up!').click()
+      cy.url().should("include", "/users/new");
+    })
+  });
+  
+  it("has a log in button", () => {
+    cy.get('form').within(($form) => {
+      cy.intercept('get', '/sessions/new')
+      cy.contains('Log in!').click()
+      cy.url().should("include", "/sessions/new");
+    })
   });
 });
